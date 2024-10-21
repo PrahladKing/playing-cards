6f10129268d78d468696786eff4d5c7a98acd6ce
Prahlad9 <prahlad@myverkoper.com>
2024-10-21 17:36:09 +0530

    added firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,15 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: 'https://dev-api.myverkoper.com/api',
+  firebaseURL: '/Cards',
+  firebase: {
+    apiKey: "AIzaSyBmZXzyl9McuNxeZmfz2rHx-kg01mOlJWo",
+    authDomain: "cards-cc636.firebaseapp.com",
+    databaseURL: "https://cards-cc636-default-rtdb.firebaseio.com",
+    projectId: "cards-cc636",
+    storageBucket: "cards-cc636.appspot.com",
+    messagingSenderId: "485568644191",
+    appId: "1:485568644191:web:5df44d55eeab38371647ea",
+    measurementId: "G-EMWEM2E489"
+  }
+};
